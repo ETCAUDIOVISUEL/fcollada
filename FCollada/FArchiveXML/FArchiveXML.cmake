d8826394c08ea533522babadcfed933a163ca69a
Thomas Pabst <thomas.pabst@gmail.com>
2016-03-15 16:04:19 +0100

    Add FArchiveXML sources

---
+++ FCollada/FArchiveXML/FArchiveXML.cmake
@@ -0,0 +1,38 @@
+set(FArchiveXML_SRC
+    FArchiveXML/FArchiveXML.cpp
+    FArchiveXML/FAXAnimationExport.cpp
+    FArchiveXML/FAXAnimationImport.cpp
+    FArchiveXML/FAXCameraExport.cpp
+    FArchiveXML/FAXCameraImport.cpp
+    FArchiveXML/FAXColladaParser.cpp
+    FArchiveXML/FAXColladaWriter.cpp
+    FArchiveXML/FAXControllerExport.cpp
+    FArchiveXML/FAXControllerImport.cpp
+    FArchiveXML/FAXEmitterExport.cpp
+    FArchiveXML/FAXEmitterImport.cpp
+    FArchiveXML/FAXEntityExport.cpp
+    FArchiveXML/FAXEntityImport.cpp
+    FArchiveXML/FAXForceFieldExport.cpp
+    FArchiveXML/FAXForceFieldImport.cpp
+    FArchiveXML/FAXGeometryExport.cpp
+    FArchiveXML/FAXGeometryImport.cpp
+    FArchiveXML/FAXImportLinking.cpp
+    FArchiveXML/FAXInstanceExport.cpp
+    FArchiveXML/FAXInstanceImport.cpp
+    FArchiveXML/FAXLightExport.cpp
+    FArchiveXML/FAXLightImport.cpp
+    FArchiveXML/FAXMaterialExport.cpp
+    FArchiveXML/FAXMaterialImport.cpp
+    FArchiveXML/FAXPhysicsExport.cpp
+    FArchiveXML/FAXPhysicsImport.cpp
+    FArchiveXML/FAXSceneExport.cpp
+    FArchiveXML/FAXSceneImport.cpp
+)
+set(FArchiveXML_H
+    FArchiveXML/FArchiveXML.h
+    FArchiveXML/FAXColladaParser.h
+    FArchiveXML/FAXColladaWriter.h
+    FArchiveXML/FAXStructures.h
+    FArchiveXML/StdAfx.cpp
+    FArchiveXML/StdAfx.h
+)
